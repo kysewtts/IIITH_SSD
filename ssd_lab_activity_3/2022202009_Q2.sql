236e694462d17da16203a451d378c775e7bb1ba8
Ujjwal Prakash <ujjwalprakash3@gmail.com>
2022-08-18 16:22:28 +0530

    Added order by for Q2

--- ssd_lab_activity_3/2022202009_Q2.sql
+++ ssd_lab_activity_3/2022202009_Q2.sql
@@ -2,7 +2,6 @@ select e.fname, e.minit, e.lname, e.ssn, temp1.dnumber, temp1.empCount from empl
 	select d.dnumber, d.dname, temp.super_ssn, temp.empCount from department d, (
 		select count(ssn) as empCount, e.super_ssn from employee e group by e.super_ssn
 	) as temp 
-	where d.mgr_ssn = temp. super_ssn
+	where d.mgr_ssn = temp.super_ssn
 ) 
-as temp1 where temp1.super_ssn = e.ssn;
-
+as temp1 where temp1.super_ssn = e.ssn order by temp1.empCount;
